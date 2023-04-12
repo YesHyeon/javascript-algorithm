e515239589d92529b3ededbbf196d68208d59cbd
shgus1224 <shgus1224@naver.com>
2023-04-12 16:46:27 +0900

    자바스크립트 2차원 배열탐색 7번 - 봉우리(수정)

--- javascript/2차원 배열탐색/7. 봉우리.js
+++ javascript/2차원 배열탐색/7. 봉우리.js
@@ -1,37 +1,37 @@
-// 봉우리
-
-// 지도 정보가 N*N 격자판에 주어집니다. 각 격자에는 그 지역의 높이가 쓰여있습니다.
-// 각 격자 판의 숫자 중 자신의 상하좌우 숫자보다 큰 숫자는 봉우리 지역입니다.
-// 봉우리 지역이 몇 개 있는 지 알아내는 프로그램을 작성하세요.
-// 격자의 가장자리는 0으로 초기화 되었다고 가정한다.
-// 만약 N=5 이고, 격자판의 숫자가 다음과 같다면 봉우리의 개수는 10개입니다
-
 function solution(arr) {
-  let answer = 0;
-  let n = arr.length;
-  let dx = [-1, 0, 1, 0];
-  let dy = [0, 1, 0, -1];
-  for (let x = 0; x < n; x++) {
-    for (let y = 0; y < n; y++) {
-      let flag = 1;
-      for (let k = 0; k < dx.length; k++) {
-        let nx = x + dx[k];
-        let ny = y + dy[k];
+  var answer = 0;
+  var xindex = [-1, 0, 1, 0];
+  var yindex = [0, 1, 0, -1];
+  var count = 0;
+  var n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      count = 0;
+      for (let z = 0; z < xindex.length; z++) {
+        var rowChange = i + xindex[z];
+        var columnChange = j + yindex[z];
         if (
-          nx >= 0 &&
-          nx < n &&
-          ny >= 0 &&
-          ny < n &&
-          arr[nx][ny] >= arr[x][y]
+          rowChange < 0 ||
+          columnChange < 0 ||
+          rowChange >= arr.length ||
+          columnChange >= arr.length
         ) {
-          flag = 0;
+          count++;
+          continue;
+        }
+
+        if (arr[rowChange][columnChange] >= arr[i][j]) {
+          count = 0;
           break;
         }
-        if (flag) answer++;
+        count++;
+      }
+      if (count == 4) {
+        answer++;
       }
     }
   }
-
   return answer;
 }
 
